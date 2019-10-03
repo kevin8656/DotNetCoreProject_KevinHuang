98d0acb5a13f0399d099cc43e7ba4d705c1ac47c
黃豊凱 <kevin8656@hotmail.com>
2019-10-03 14:17:54 +0800

    Initial Commit

---
+++ DotNetCoreProject_KevinHuang/Models/Member.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetCoreProject_KevinHuang.Models
+{
+    public partial class Member
+    {
+        public int Id { get; set; }
+        public string Account { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ DotNetCoreProject_KevinHuang/Models/dotnetcoreContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DotNetCoreProject_KevinHuang.Models
+{
+    public partial class dotnetcoreContext : DbContext
+    {
+        public dotnetcoreContext()
+        {
+        }
+
+        public dotnetcoreContext(DbContextOptions<dotnetcoreContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Member> Member { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=localhost;user id=dotnetcore;password=dotnetcore; persist security info=True; Initial Catalog=dotnetcore;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Member>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Account)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+
+                entity.Property(e => e.Name).HasMaxLength(30);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Phone)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .IsFixedLength();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
