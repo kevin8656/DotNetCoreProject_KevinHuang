564a10ca6162295d459abc520f57957fc9efc2ba
kevinhuang8656 <kevin86568656@gmail.com>
2019-10-04 18:10:57 +0800

    1.新增MemberController，增加取得所有Member的API 2.調整run專案的預設路徑為api/member 3.新增MemberService，撈取所有Member資料

---
+++ DotNetCoreProject_KevinHuang/Controllers/MemberController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetCoreProject_KevinHuang.Models;
+using DotNetCoreProject_KevinHuang.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreProject_KevinHuang.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        private MemberService _memberservice = new MemberService();
+        [HttpGet]
+        public List<Member> Get()
+        {
+            
+            return _memberservice.GetMembers(); ;
+        }
+    }
+}
---
+++ DotNetCoreProject_KevinHuang/Services/MemberService.cs
@@ -0,0 +1,17 @@
+﻿using DotNetCoreProject_KevinHuang.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreProject_KevinHuang.Services
+{
+    public class MemberService
+    {
+        private dotnetcoreContext _context = new dotnetcoreContext();
+        public List<Member> GetMembers()
+        {
+            return _context.Member.ToList();
+        }
+    }
+}
